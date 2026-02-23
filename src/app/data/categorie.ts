c2f72abc0c2a13c6b2a1c091fd2656a4294d8452
godafflig <tombenat99@hotmail.fr>
2026-02-23 09:48:43 +0100

    back-end

---
+++ src/app/data/categorie.ts
@@ -0,0 +1,11 @@
+export type Categorie = {
+    id: number;
+    name: string;
+};
+
+export const categories: Categorie[] = [
+    { id: 1, name: "Electronics" },
+    { id: 2, name: "Clothing" },
+    { id: 3, name: "Books" },
+    { id: 4, name: "Home & Kitchen" },
+];
